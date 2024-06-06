824221a9143fa4a27ace3eaae8e3c5b9dcb2ff97
RobGlob <dizbasarov73@gmail.com>
2024-06-06 18:33:34 +0500

    Update converter.py
    It now resizes the image in addition to converting it

--- main/converter.py
+++ main/converter.py
@@ -1,35 +1,52 @@
 import cv2
 import os
-
-"""Данный код нужен для преобразования цветной фотки в черно-белое (в 1 цветовой канал)
-Если нужны подробности свяжитесь с diasizbasarov123@gmail.com"""
+import imghdr
 
 
 def convert_to_grayscale(input_path, output_folder):
-    # провекри и обработки исключении
+    """
+    Convert color images to grayscale and save them in the specified output folder.
+    If you have any questions please contact diasizbasarov123@gmail.com
+
+    Args:
+        input_path (str): Path to the directory containing input images.
+        output_folder (str): Path to the directory where grayscale images will be saved.
+    """
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
+
+    if not os.path.exists(input_path):
+        print(f"Input directory {input_path} does not exist.")
+        return
 
     for filename in os.listdir(input_path):
         file_path = os.path.join(input_path, filename)
 
-        if not os.path.isfile(file_path) or not filename.lower().endswith(('.png', '.jpg', '.jpeg', '.bmp', '.tiff')):
+        if not os.path.isfile(file_path):
+            continue
+
+        # Check if the file is an image
+        if imghdr.what(file_path) is None:
+            print(f"{file_path} is not a valid image file.")
             continue
 
         image = cv2.imread(file_path)
 
         if image is None:
-            print(f"Ошибка: не удалось открыть изображение {file_path}")
+            print(f"Failed to open the image {file_path}")
             continue
 
-        gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)  # преобразование в черно-белое
+        gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)  # Convert to grayscale
 
-        # Полный путь к выходному файлу
+        # Resize the image to 128x128 pixels
+        resized_image = cv2.resize(gray_image, (128, 128))
+
+        # Full path to the output file
         output_path = os.path.join(output_folder, filename)
 
-        cv2.imwrite(output_path, gray_image)
+        cv2.imwrite(output_path, resized_image)
 
-        print(f"Изображение {file_path} успешно преобразовано и сохранено в {output_path}")
+        print(f"Image {file_path} successfully transformed, resized to 128x128, and saved in {output_path}")
 
 
 input_path = 'faces'
